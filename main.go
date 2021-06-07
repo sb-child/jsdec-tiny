bcf09fca277d3ca312bced244273b7f9861dc5da
sbchild <a15315862177@163.com>
2021-06-07 09:13:36 +0800

    error 提示

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -11,6 +12,7 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
+		fmt.Println("error: 需要参数")
 		return
 	}
 	inp := os.Args[1]
@@ -18,25 +20,30 @@ func main() {
 	out, err := base64.StdEncoding.DecodeString(inp)
 	outStr := string(out)
 	if err != nil {
+		fmt.Println("error: base64解码失败")
 		panic(err)
 	}
 	file, err := os.Open("dec.js")
 	if err != nil {
+		fmt.Println("error: 无法打开 dec.js")
 		panic(err)
 	}
 	defer file.Close()
 	fileByte, err := ioutil.ReadAll(file)
 	if err != nil {
+		fmt.Println("error: 无法读取 dec.js")
 		panic(err)
 	}
 	fileString := string(fileByte)
 	vm := otto.New()
 	_, err = vm.Run(fileString)
 	if err != nil {
+		fmt.Printf("error: 加载 dec.js 时出错: %s", err.Error())
 		panic(err)
 	}
 	ret, err := vm.Call("autoscan", nil, outStr)
 	if err != nil {
+		fmt.Printf("error: 解密时出错: %s", err.Error())
 		panic(err)
 	}
 	print(ret.String())
